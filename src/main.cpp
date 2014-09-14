724040c5fa1a63af54d15dbbd03f6b6fcf405a40
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-09-14 15:25:55 +0200

    src

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,42 @@
+#include <SDL.h>
+#include <SDL_image.h>
 #include <iostream>
-using namespace std;
+#include <SDL/SDL.h>
+
+static SDL_Window *window;
+
+static void eventLoop() {
+	SDL_Event ev;
+	while(SDL_WaitEvent(&ev)) {
+		switch(ev.type) {
+			case SDL_QUIT:
+				return;
+			default:
+				break;
+		}
+	}
+}
 
 int main() {
-  cout << "Hello, World!" << endl;
-  return 0;
+	if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cerr << "error on SDL_Init: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	window = SDL_CreateWindow(
+			"ImageViewer",
+			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			640, 480,
+			0);
+	if(!window) {
+		std::cerr << "error on creating window: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	eventLoop();
+
+	SDL_DestroyWindow(window);
+
+	return 0;
 }
+
